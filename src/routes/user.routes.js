4e0b26cbf88c15072100af3497b3d44201f2ea43
jangrarajat <rajatjangra940@gmail.com>
2025-09-01 20:08:30 +0530

    feat: enhance user routes with additional account management and profile features

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loggoutUser, loginUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loggoutUser, loginUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -21,9 +21,31 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser)
 
-router.route("/logout").post(verifyJwt,loggoutUser)
+router.route("/logout").post(verifyJwt, loggoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/reset-password").post(verifyJwt, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+
+router.route("/update-account").patch(verifyJwt, updateAccountDetails)
+
+router.route("/update-avatar").patch(verifyJwt, upload.single("avater"), updateUserAvatar)
+
+router.route("/update-cover-image").patch(verifyJwt, upload.single("/coverImage"), updateUserCoverImage)
+
+router.route("/update-avatar").patch(verifyJwt, upload.single("avater"), updateUserAvatar)
+
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile)
+
+router.route("/watch-history").patch(verifyJwt, getWatchHistory)
+
+
+
+
+
+
 
 
 export default router
