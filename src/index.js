a0b27b2ab1a51f0ffbabd12537f59809b9a8a4af
jangrarajat <rajatjangra940@gmail.com>
2025-09-05 22:12:55 +0530

    index.js file remove 2 import line

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import dotenv from "dotenv"
 dotenv.config()
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 import {app} from "./app.js"
 
